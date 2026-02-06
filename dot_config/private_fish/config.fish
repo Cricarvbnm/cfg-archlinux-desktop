7ce7ef1379992cabd299c5bd93a6973139b828a8
Alec <alechron@outlook.com>
2026-02-07 00:01:04 +0800

    feat(fish)

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,36 @@
+# Ensure SHELL variable is set to fish
+set -gx SHELL "/usr/bin/fish"
+
+status is-login; and begin
+
+    # Login shell initialisation
+    # Source /etc/profile if it exists
+    exec bash -c "test -e /etc/profile && source /etc/profile; exec fish"
+
+end
+
+status is-interactive; and begin
+
+    # Abbreviations
+    abbr --add -- cpr 'rsync --archive -hhh --partial --info=stats1,progress2 --modify-window=1'
+    abbr --add -- mvr 'rsync --archive -hhh --partial --info=stats1,progress2 --modify-window=1 --remove-source-files'
+
+    # Aliases
+    alias cat 'bat --plain'
+    alias cd z
+    alias diff 'git diff --no-index'
+    alias l 'ls -Al'
+    alias la 'ls -A'
+    alias ll 'ls -l'
+    alias ls lsd
+    alias tree 'lsd --tree'
+
+    # Interactive shell initialisation
+    # Use vi keybindings
+    set --global fish_key_bindings fish_vi_key_bindings
+
+    zoxide init fish | source
+
+    starship init fish | source
+
+end
