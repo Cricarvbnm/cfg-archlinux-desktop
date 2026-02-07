27f7bb9a83bc9f87f918f1b3d6285f842409fb5c
Alec <alechron@outlook.com>
2026-02-07 22:21:38 +0800

    refactor(fish/greeting): Use `fish_greeting` to implement welcome msg

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,18 @@
-status is-login; and begin
-  echo "Login $SHELL: Welcome $USER"
+function fish_greeting
+  status is-login; and begin
+    echo "Login $SHELL: Welcome $USER"
+  end
+
+  status is-interactive; and begin
+    echo "Interactive $SHELL: Welcome $USER"
+  end
 end
 
+status is-login; and begin
+end
 
 status is-interactive; and begin
 
   set --global fish_key_bindings fish_vi_key_bindings
 
-  echo "Interactive $SHELL: Welcome $USER"
 end
