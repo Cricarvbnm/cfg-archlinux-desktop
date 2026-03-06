11e44fdc200666cdc6765aa16834a5f3faf47e6b
Alec <alechron@outlook.com>
2026-03-06 20:29:57 +0800

    feat(fish/completion): openclaw

---
+++ dot_config/private_fish/completions/openclaw.fish
@@ -0,0 +1,2 @@
+# OpenClaw Completion
+source "/home/alec/.openclaw/completions/openclaw.fish"
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,11 @@
 function fish_greeting
-  status is-login; and begin
-    echo "Login $SHELL: Welcome $USER"
-  end
+    status is-login; and begin
+        echo "Login $SHELL: Welcome $USER"
+    end
 
-  status is-interactive; and begin
-    echo "Interactive $SHELL: Welcome $USER"
-  end
+    status is-interactive; and begin
+        echo "Interactive $SHELL: Welcome $USER"
+    end
 end
 
 status is-login; and begin
@@ -13,10 +13,10 @@ end
 
 status is-interactive; and begin
 
-  set --global fish_key_bindings fish_vi_key_bindings
+    set --global fish_key_bindings fish_vi_key_bindings
 
 end
 
 for config in $XDG_CONFIG_HOME/fish/config.d/*.fish
-  source $config
+    source $config
 end
