f023b458a2eccc8e03a7538a361fd57211f8c33a
Alec <alechron@outlook.com>
2026-03-17 10:01:05 +0800

    feat(fish/function/git-push-all): Add some user-friendly outputs

--- dot_config/private_fish/config.d/50-functions.fish
+++ dot_config/private_fish/config.d/50-functions.fish
@@ -1,7 +1,9 @@
 status is-interactive; and begin
     function git-push-all
         for remote in (git remote)
+            echo Normal pushing to $remote
             git push $remote
+            echo Pushing tags to $remote
             git push $remote --tags
         end
     end
